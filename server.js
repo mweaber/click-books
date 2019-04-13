d7e2b31838427ed32d680b30a61d521308b68a62
Matthew Weaber <matthewweaber89@gmail.com>
2019-04-13 16:09:51 -0400

    Remaking initial file

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes")
-// const path = require("path");
-const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -14,16 +12,12 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
-app.use(routes);
-
-//Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/readingRailroad");
 
 // Send every other request to the React app
 // Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
